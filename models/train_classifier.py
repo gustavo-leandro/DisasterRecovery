105bee7f46d6dfec849f7a18d4b9b97b5149a35f
Gustavo Leandro <gustavo.sleandro@gmail.com>
2021-07-18 12:51:11 -0300

    using grid search for better parameters

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -72,7 +72,7 @@ def tokenize(text):
 def build_model():
     '''
     OUTPUT:
-    pipeline - pipeline model to be used
+    cv - model with the best parameters combination
 
     '''
 
@@ -81,8 +81,19 @@ def build_model():
         ('tfidf', TfidfTransformer()),
         ('clf', MultiOutputClassifier(RandomForestClassifier()))
         ])
+
+    parameters = {
+        'vect__ngram_range': ((1, 1), (1, 2)),
+        'vect__max_df': (0.5, 0.75, 1.0),
+        'vect__max_features': (None, 5000, 10000),
+        'tfidf__use_idf': (True, False),
+        'clf__estimator__n_estimators': [50, 100, 200],
+        'clf__estimator__min_samples_split': [2, 3, 4],
+    }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters)
     
-    return pipeline
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
